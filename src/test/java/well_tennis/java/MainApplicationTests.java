8fd0b00599c1fb7984710adf0d4e2b6e2d68a281
Thomas M <micheli17u@etu.univ-lorraine.fr>
2024-11-27 10:28:17 +0100

    IT1 - start

---
+++ src/test/java/well_tennis/java/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package well_tennis.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
