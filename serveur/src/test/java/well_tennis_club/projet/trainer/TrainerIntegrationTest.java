5bfb9e6afb32d0d363e378bafdde50f8d6c9e5a0
matheo <matheo.pedron57@gmail.com>
2025-02-12 09:22:06 +0100

    Correction bug

--- serveur/src/test/java/well_tennis_club/projet/trainer/TrainerIntegrationTest.java
+++ serveur/src/test/java/well_tennis_club/projet/trainer/TrainerIntegrationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import well_tennis_club.projet.WellTennisClubApplication;
+import well_tennis_club.WellTennisClubApplication;
 
 import java.util.UUID;
 
